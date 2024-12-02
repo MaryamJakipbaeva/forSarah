d5489bdbb5c18c263a1e56d6ed2ee8aa66f65d3a
Bibimaryam Jakipbaeva <146994580+MaryamJakipbaeva@users.noreply.github.com>
2024-12-02 18:19:30 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import HeaderView from './components/Header/HeaderView';
+import HomeView from './views/HomeView';
+import SignInView from './views/SignInView';
+import MoviesView from './views/MoviesView';
+import GroupsView from './views/GroupsView';
+import MovieDetails from './views/MovieDetailsView/MovieDetails';
+import Footer from './components/Footer/Footer'
+import LogoutView from './views/LogoutView';
+import ShowtimesView from './views/ShowtimesView1/ShowtimesView';
+
+
+export default function App() {
+
+  return (
+    <div className="App bg-dark text-white">
+      <Router>
+        <HeaderView />
+
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/sign-in" element={<SignInView />} />
+          <Route path="/movies" element={<MoviesView />} />
+          <Route path="/groups" element={<GroupsView />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/logout" element={<LogoutView />} />
+          <Route path="/showtimes" element={<ShowtimesView />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+  
+}
+
